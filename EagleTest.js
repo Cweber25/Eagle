87ecccaef86e9c5e1d4d037287ec50644c01c510
ksundb <ksundberg11@gmail.com>
2022-11-10 04:25:42 -0500

    Working on Fail

--- EagleTest.js
+++ EagleTest.js
@@ -4,7 +4,7 @@ function changeImage() {
   if (answer == "Rec") {
       document.getElementsByClassName('popup')[0].style.visibility = 'visible';
   } else if(count > 5) {
-    
+    document.getElementsByClassName('popup1')[0].style.visibility = 'visible';
   } else {
     document.getElementById('img').height = document.getElementById('img').height - 50;
     document.getElementById('img').width = document.getElementById('img').width - 50;
