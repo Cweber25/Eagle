8419f0a0d600cc99e29029a66f976647f325b2d9
Clay Atkinson <clayatkinson78@gmail.com>
2022-11-17 16:55:21 -0500

    group meeting from nov 17th 4:55pm

--- EagleTest.js
+++ EagleTest.js
@@ -1,13 +1,30 @@
 var count = 0;
+var array = [
+  {
+  answer: 'rec',
+  Image: "/images/00_rec.JPG"
+  },
+  {
+    answer: 'Niss',
+    Image: "/images/00_niss-interior.JPG"
+  }
+];
+
+function myFunction(){
+  let a = 0;
+
+  document.getElementById("img").src = array[a][Image];
+}
+
 function changeImage() {
   var answer = document.getElementById("answer").value;
   if (answer == "Rec") {
       document.getElementsByClassName('popup')[0].style.visibility = 'visible';
-  } else if(count > 5) {
+  } else if(count > 4) {
     document.getElementsByClassName('popup1')[0].style.visibility = 'visible';
   } else {
-    document.getElementById('img').height = document.getElementById('img').height - 50;
-    document.getElementById('img').width = document.getElementById('img').width - 50;
+    document.getElementById('img').height = document.getElementById('img').height - 200;
+    document.getElementById('img').width = document.getElementById('img').width - 200;
     count++
   }
 }
